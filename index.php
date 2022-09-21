2aef5ed5dd57496cb83fa7dfdcad929e5281fb9f
jervz09 <jervyariola@gmail.com>
2022-09-21 20:43:35 +0800

    feat: including extra about us file

--- index.php
+++ index.php
@@ -23,49 +23,7 @@
     <!-- ##### About Us Area End ##### -->
 
     <!-- ##### Extra About Us Area Start ##### -->
-    <section class="section-padding-100 bg-img bg-fixed" style="background-image: url(img/bg-img/4.jpg);">
-      <div class="container">
-        <div class="row justify-content-end">
-          <div class="col-12 col-lg-7">
-            <div class="about-2-content text-center wow fadeInUp" data-wow-delay="300ms">
-              <div class="section-heading text-center white">
-                <div class="line-"></div>
-                <h2>What Are You Waiting For?</h2>
-                <p>Bring Out Your Family, LoveOnes, Barkada, Tropa, Work Mates At Kapitbahay Na Mga Ka - 𝖧𝗈𝗍𝗌𝗊𝗎𝖺𝖽.</p>
-              </div>
-              <div class="row">
-                <div class="col-12 col-sm-3">
-                  <div class="about-2-feature">
-                    <img src="img/abt-img/beer.png">
-                    <p>Beer</p>
-                  </div>
-                </div>
-                <div class="col-12 col-sm-3">
-                  <div class="about-2-feature">
-                    <img src="img/abt-img/billiard.png">
-                    <p>Billiard</p>
-                  </div>
-                </div>
-                <div class="col-12 col-sm-3">
-                  <div class="about-2-feature">
-                    <img src="img/abt-img/music.png">
-                    <p>Music Band</p>
-                  </div>
-                </div>
-                <div class="col-12 col-sm-3">
-                  <div class="about-2-feature">
-                    <img src="img/abt-img/dine.png">
-                    <p>Dine</p>
-                  </div>
-                </div>
-              </div>
-              <!-- Button -->
-              <a href="#" class="btn palatin-btn mt-50">Read More</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <?php include('php/extra_about_us.php')?>
     <!-- ##### Extra About Us Area End ##### -->
 
     <!-- ##### Contact Area Start ##### -->
