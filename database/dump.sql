2b9a22f3708840e0b0c73980a21f51c3cce9c96a
jervz09 <jervyariola@gmail.com>
2022-09-23 14:15:38 +0800

    chore: update db schema for contact list

--- database/dump.sql
+++ database/dump.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `users` (
     `user_id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `username` VARCHAR(100) NOT NULL,
-    `first_name` VARCHAR(100) NOT NULL,
-    `middle_name` VARCHAR(100) NOT NULL,
-    `last_name` VARCHAR(100) NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `first_name` VARCHAR(50) NOT NULL,
+    `middle_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
-    `contact_no` VARCHAR(100) NOT NULL,
+    `contact_no` VARCHAR(12) NOT NULL,
     `verified` TINYINT(1) NOT NULL DEFAULT '0',
     `token` VARCHAR(255) DEFAULT NULL,
     `password` VARCHAR(255) NOT NULL,
-    `role` VARCHAR(5) NOT NULL
+    `role_id` TINYINT(1) NOT NULL DEFAULT '0'
 );
 
 CREATE TABLE IF NOT EXISTS `table_list` (
     `table_id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `tbl_no` INTEGER NOT NULL,
+    `table_no` INTEGER NOT NULL,
     `name` INTEGER NOT NULL,
     `description` INTEGER NOT NULL,
     `coordinates` TEXT NOT NULL,
@@ -25,10 +25,25 @@ CREATE TABLE IF NOT EXISTS `table_list` (
 
 CREATE TABLE IF NOT EXISTS `reservation_list` (
     `reservation_id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `user_id` TEXT NOT NULL,
+    `user_id` INT(11) NOT NULL,
     `table_id` INTEGER NOT NULL,
     `datetime` TIMESTAMP NOT NULL,
     `status` INTEGER NOT NULL DEFAULT 0,
     `date_created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `is_delete` INTEGER NOT NULL DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS `role_list` (
+    `role_id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `role_name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `contact_list` (
+    `contact_id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `address` VARCHAR(50) NOT NULL,
+    `phone number` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `facebook_link` VARCHAR(50) NOT NULL,
+    `twitter_link` VARCHAR(50) NOT NULL
+);
+
