70efdbf9bd7633c2696ab51dae0d6c85f3b0e0e1
jervz09 <jervyariola@gmail.com>
2022-10-27 10:31:46 +0800

    feat: change title name for login and register

--- head_form.php
+++ head_form.php
@@ -12,7 +12,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title>Hotside - Login</title>
+	<title>Hotside - Welcome</title>
 
 	<!-- Custom fonts for this template-->
 	<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
